a7f44e34f83cf066c79f21cf0db7d2d59b082916
Robert Ryan Thompson <baronretry@proton.me>
2024-06-28 23:52:07 -0600

    File and UI hygiene
    Tweak init.el to use modus-themes explicitly, turn on saved minibuffer history, put backups and autosaves in a single location, start using use-package, and change the default font.

--- init.el
+++ init.el
@@ -1,3 +1,6 @@
+(eval-when-compile
+  (require 'use-package))
+
 (require 'server)
 (unless (server-running-p)
   (server-start)) 
@@ -6,19 +9,28 @@
       visible-bell t)
 
 
+;; Turn different modes off and on
+
 ;; (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (global-display-line-numbers-mode 1)
+(savehist-mode 1)
+
+;; Change location of backups and autosaves
+
+(setq temporary-file-directory "C:/LocalData/")
+
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+
+(use-package ess-site)
+(use-package modus-themes)
 
 (load-theme 'modus-vivendi t)
-
-
-
-
-
-
-
 
 
 (custom-set-variables
@@ -26,10 +38,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(ess)))
+ '(global-display-line-numbers-mode t)
+ '(package-selected-packages '(modus-themes ess))
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Inconsolata Nerd Font" :foundry "outline" :slant normal :weight regular :height 100 :width normal)))))
