582c87ab663122b8436a99754d76510955c331e3
Robert Ryan Thompson <baronretry@proton.me>
2024-07-01 23:19:09 -0600

    Minor cleanup
    Minor cleanup of redundant variables added by Custom.

--- init.el
+++ init.el
@@ -32,15 +32,6 @@
 
 (load-theme 'modus-vivendi t)
 
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(global-display-line-numbers-mode t)
- '(package-selected-packages '(modus-themes ess))
- '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
