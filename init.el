221c06b653781ffee60c4790eef436ce4356ddfa
Robert Ryan Thompson <baronretry@proton.me>
2025-11-22 23:52:59 -0700

    Use LSP/tree-sitter and treemacs; start using use-package

--- init.el
+++ init.el
@@ -1,6 +1,26 @@
 ;;; -*- lexical-binding: t -*-
 
-(custom-set-variables
- '(custom-enabled-themes '(modus-vivendi-tinted)))
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+(use-package lsp-mode)
+(use-package tree-sitter)
+(use-package tree-sitter-langs)
+(use-package treemacs)
+(use-package lsp-treemacs)
 
 (savehist-mode 1)
+(global-ede-mode t)
+
+(treemacs)
+(load-theme 'modus-vivendi-tinted)
+
+(custom-set-variables
+ '(package-selected-packages
+   '(lsp-docker lsp-java lsp-mode lsp-pyright lsp-treemacs
+		project-treemacs treeview)))
+
+
+
